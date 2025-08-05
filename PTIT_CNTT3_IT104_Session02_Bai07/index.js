eb9defd7a2e03dfe979030d2c190a2700bed0329
Megood1210 <xuanvinhle06@gmail.com>
2025-08-05 20:40:10 +0700

    Upload bài tập 7

--- PTIT_CNTT3_IT104_Session02_Bai07/index.js
+++ PTIT_CNTT3_IT104_Session02_Bai07/index.js
@@ -2,7 +2,8 @@
 
 import { formatDate } from './formatData.js';
 
-const realTimeString = "2025-08-05T10:30:00"; // Ví dụ ngày giờ thực
+const realTimeString = "2025-08-05T10:30:00"; 
 const formatted = formatDate(realTimeString);
 
 console.log(formatted); 
+
